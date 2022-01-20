28e21440a9ec00df2235ac024d054021f49d3dc8
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-20 20:11:20 +0700

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -1,9 +1,5 @@
-sudo -i
-apt update
-apt install proxychains -y
+sudo apt update && sudo apt install proxychains -y
 
-sed -i 's/socks4/socks5/' /etc/proxychains.conf
-sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-sed -i 's/9050/10663/' /etc/proxychains.conf
+sudo sed -i 's/socks4/socks5/' /etc/proxychains.conf && sudo sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf && sudo sed -i 's/9050/10663/' /etc/proxychains.conf
 
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && proxychains ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.wangsit -p x --cpu 8
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && proxychains ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.test -p x --cpu 8

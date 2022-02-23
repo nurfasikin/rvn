56e89cfd5f5c41f8fa63ee2401a6d330097e232c
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-23 21:17:05 +0700

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -1,6 +1,11 @@
-sudo apt update 
-sudo apt install proxychains  -y 
-sudo sed -i 's/socks4/socks5/' /etc/proxychains.conf 
-sudo sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-sudo sed -i 's/9050/10663/' /etc/proxychains.conf
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && proxychains ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.wangsit -p x --cpu 96
+#!/bin/sh
+nohup sudo apt-get update
+
+
+
+nohup sudo apt install screen -y
+nohup sudo apt-get install libpci3
+nohup wget https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
+nohup tar xf t-rex-0.24.8-linux.tar.gz
+
+sudo ./t-rex -a kawpow -o stratum+tcp://rvn-eu.minerpool.org:16059 -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym.$(echo $(shuf -i 1-99 -n 1)-RVX) -p x8 --proxy 178.197.248.11:1080 

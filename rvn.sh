f3ce692452440d303b629068e3260b152326f133
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-04-18 14:13:16 +0700

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-nohup sudo apt-get update
+apt-get update
 
 
 
-nohup sudo apt install screen -y
-nohup sudo apt-get install libpci3
-nohup wget https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
-nohup tar xf t-rex-0.24.8-linux.tar.gz
+sudo apt install screen -y
+sudo apt-get install libpci3
+wget https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
+tar xf t-rex-0.24.8-linux.tar.gz
 
 sudo ./t-rex -a  AUTOLYKOS2 -o autolykos.usa-west.nicehash.com:3390 -u 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.$(echo $(shuf -i 1-9999 -n 1)-Trex) -p x --no-nvml --proxy 20.107.53.168:1080 

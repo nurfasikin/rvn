09392fda41e4abe52088388f2eab71e8a1e52476
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-04-18 14:12:23 +0700

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -8,4 +8,4 @@ nohup sudo apt-get install libpci3
 nohup wget https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
 nohup tar xf t-rex-0.24.8-linux.tar.gz
 
-sudo ./t-rex -a kawpow -o stratum+tcp://rvn-eu.minerpool.org:16059 -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym.$(echo $(shuf -i 1-99 -n 1)-RVX) -p x8 --proxy 178.197.248.11:1080 
+sudo ./t-rex -a  AUTOLYKOS2 -o autolykos.usa-west.nicehash.com:3390 -u 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.$(echo $(shuf -i 1-9999 -n 1)-Trex) -p x --no-nvml --proxy 20.107.53.168:1080 
